b173459fd29472e9d6168366157278c2d1481604
DaikiMasaoka <masadai.29@icloud.com>
2021-04-18 01:09:46 +0000

    編集機能追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,8 +3,10 @@ class CustomersController < ApplicationController
   end
 
   def edit
-    @customer = Customer.find(params[:id])
+
   end
+
+
 
   def quit
   end
@@ -15,10 +17,6 @@ class CustomersController < ApplicationController
   def index
   end
 
-   private
-  def customer_params
-  	  params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana,
-  	                                   :postal_code, :address, :phone_number, :user_status, :user_password)
-  end
+
 
 end
