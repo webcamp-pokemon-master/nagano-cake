dd882e17c552b7714adbff3a42bb278bfbb8d88c
pkoky <sscsk2224@gmail.com>
2021-04-19 01:45:23 +0000

    ジャンル編集機完成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,12 +7,18 @@ class Admin::GenresController < ApplicationController
   def create
     genre = Genre.new(genre_params)
     genre.save
-    redirect_to admin_homes_top_path
+    redirect_to admin_genres_path
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
 
   private
   def genre_params
