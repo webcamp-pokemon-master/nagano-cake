159fe463eb7e0211c00b056cf4fe4d3cb8daad69
ninunu7 <pibf51@gmail.com>
2021-04-20 04:24:41 +0000

    cartsum

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -2,6 +2,10 @@ class Customer::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
     @sum = 0
+    @cart_items.each do |cart_item|
+      @subtotal = (Product.find(cart_item.product_id).price * 1.1 * cart_item.amount).to_i
+      @sum += @subtotal
+    end
   end
 
   def create
