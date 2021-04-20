9cc0d4162b33e750b4a970f463437beca5d8b0e8
pkoky <sscsk2224@gmail.com>
2021-04-20 06:06:16 +0000

    add'

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -12,15 +12,23 @@ class Customer::OrdersController < ApplicationController
   end
 
   def comfirm
-    @order params
     
+    @order = Order.new
+    # 空のインスタンスを作成
+    
+    # @order.address = current_customer.address
+    if params[:order][:selected_address] == 1
+      
+    elsif params[:order][:selected_address] == 2
+      
+    elsif params[:order][:selected_address] == 3
 
     # @order = Order.find(params[:order_id])
     # @address = @order.address.new(order_params)
 
-      # @order.postal_code = current_customer.postal_code
-      # @order.address     = current_customer.address
-      # @order.name        = current_customer.last_name + current_customer.first_name
+    # @order.postal_code = current_customer.postal_code
+    # @order.address     = current_customer.address
+    # @order.name        = current_customer.last_name + current_customer.first_name
   end
 
   def show
@@ -32,7 +40,7 @@ class Customer::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:name, :address, :postal_code, :payment_method)
+    params.require(:order).permit(:name, :address, :postal_code, :payment_method ,:selected_address)
   end
 
 
