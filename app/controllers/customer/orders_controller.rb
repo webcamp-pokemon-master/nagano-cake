0e32b0a9920f8e706a0407062d299f81c3960062
pkoky <sscsk2224@gmail.com>
2021-04-22 06:10:15 +0000

    add

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,4 +1,5 @@
 class Customer::OrdersController < ApplicationController
+  helper_method :nameaddress
   def index
     @orders = current_customer.orders
     # or
@@ -6,43 +7,47 @@ class Customer::OrdersController < ApplicationController
   end
 
   def new
+
     @order = Order.new
     @delivery_addresses = DeliveryAddress.where(customer: current_customer)
+
+
   end
 
 
   def comfirm
-    @cart_items = current_customer.cart_items.all
-    @ordersnew = Order.new
-    @order = Order.new(customer: current_customer)
-    # 空のインスタンスを生成し、customerカラムにcurrento_customerを入れている
-    @order.payment_method = params[:order][:payment_method]
-    # 上記２つは下記記述でもokay
-    # @order = Order.new(customer: current_customer, payment_method: params[:order][:payment_method])
-    if params[:order][:selected_address] == "1"
-      # "1"はviewでは整数で情報を送っているが、urlで情報を送る際に全て文字になるためここでは""をつける。
-      @order.address     = current_customer.address
-      @order.name        = current_customer.last_name + current_customer.first_name
-      @order.postal_code = current_customer.postal_code
+      @cart_items = current_customer.cart_items.all
+      @ordersnew = Order.new
+      @order = Order.new(customer: current_customer)
+      # 空のインスタンスを生成し、customerカラムにcurrento_customerを入れている
+      @order.payment_method = params[:order][:payment_method]
+      # 上記２つは下記記述でもokay
+      # @order = Order.new(customer: current_customer, payment_method: params[:order][:payment_method])
+      if params[:order][:selected_address] == "1"
+        # "1"はviewでは整数で情報を送っているが、urlで情報を送る際に全て文字になるためここでは""をつける。
+        @order.address     = current_customer.address
+        @order.name        = current_customer.last_name + current_customer.first_name
+        @order.postal_code = current_customer.postal_code
 
-    elsif params[:order][:selected_address] == "2"
-      address = DeliveryAddress.find(params[:order][:delivery_address_id])
-      @order.address     = address.address
-      @order.postal_code = address.postal_code
-      @order.name        = address.name
+      elsif params[:order][:selected_address] == "2"
+        address = DeliveryAddress.find(params[:order][:delivery_address_id])
+        @order.address     = address.address
+        @order.postal_code = address.postal_code
+        @order.name        = address.name
 
-    elsif params[:order][:selected_address] == "3"
-      @order.postal_code = params[:order][:postal_code]
-      @order.address     = params[:order][:address]
-      @order.name        = params[:order][:name]
-    end
+      elsif params[:order][:selected_address] == "3"
+        @order.postal_code = params[:order][:postal_code]
+        @order.address     = params[:order][:address]
+        @order.name        = params[:order][:name]
+      end
 
-    @sum = 0
-    @cart_items.each do |cart_item|
-      @subtotal = (Product.find(cart_item.product_id).price * 1.1 * cart_item.amount).to_i
-      @sum += @subtotal
-    end
+      @sum = 0
+      @cart_items.each do |cart_item|
+        @subtotal = (Product.find(cart_item.product_id).price * 1.1 * cart_item.amount).to_i
+        @sum += @subtotal
+      end
   end
+
 
   def create
     # @order = current_customer.orders.new(name: params[:order][:name],
@@ -79,10 +84,10 @@ class Customer::OrdersController < ApplicationController
 
   def nameaddress
     @delivery_addresses = DeliveryAddress.where(customer: current_customer)
-    "#{@delivery_addresses.name}. #{@delivery_addresses.address}"
+    return @delivery_addresses.name
   end
 
-  helper_method :nameaddress
+
 
   private
   def order_params
