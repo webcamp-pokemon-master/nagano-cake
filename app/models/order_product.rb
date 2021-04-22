a65d258d73653551dccda6e989437d2d8f813b84
pkoky <sscsk2224@gmail.com>
2021-04-22 02:51:59 +0000

    enum 制作ステータスokay

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,7 +1,7 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
-  
-  enum making_status: {cannot_create: 0, confirm_payment: 1,under_construction: 2, ready_to_ship: 3, shipped: 4}
+
+  enum making_status: {cannot_create: 0, waiting_create: 1, in_production: 2, completed: 3}
 
 end
