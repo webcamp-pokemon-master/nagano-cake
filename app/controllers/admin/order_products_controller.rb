ae1efaa43b59cefb19ac1efb8ee16141533c2433
DaikiMasaoka <masadai.29@icloud.com>
2021-04-23 13:13:00 +0000

    注文詳細

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -1,13 +1,13 @@
 class Admin::OrderProductsController < ApplicationController
   def update
-  @order_product = OrderProduct.find(params[:id])
+  order_product = OrderProduct.find(params[:id])
   order_product.update(order_product_params)
-  redirect_to admin_order_path(oeder_product.order)
+  redirect_to request.referer
   end
 
   private
   def order_product_params
-    params.require(:order_item).permit(:making_status)
+    params.require(:order_product).permit(:making_status)
   end
 
 end
