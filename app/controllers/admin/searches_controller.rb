4ff9e9ebee8cdeb5f0562b5fba6ff7dc7463ae3d
DaikiMasaoka <masadai.29@icloud.com>
2021-04-26 03:01:43 +0000

    検索機能完成

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -4,10 +4,7 @@ class Admin::SearchesController < ApplicationController
   def search
   	@range = params[:range]
 		@content = params[:content]
-		if @range == "会員"
-			@customers = Customer.search_for(@content)
-		else
-			@products = Product.search_for(@content)
-		end
+		@customers = Customer.search_for(@content)
+		@products = Product.search_for(@content)
   end
 end
