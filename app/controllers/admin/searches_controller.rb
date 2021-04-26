5eec937ca952deb0a2150db9ca38ed4160e69d49
pkoky <sscsk2224@gmail.com>
2021-04-26 03:02:02 +0000

    検索機能完成

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -4,10 +4,10 @@ class Admin::SearchesController < ApplicationController
   def search
   	@range = params[:range]
 		@content = params[:content]
-		if @range == "会員"
+		# if @range == "会員"
 			@customers = Customer.search_for(@content)
-		else
+		# else
 			@products = Product.search_for(@content)
-		end
+		# end
   end
 end
